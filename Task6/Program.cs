ea7f9d57c05b81347f4ec1c137b1323befb7fb3b
Konstantin Chern <codahl@yandex.ru>
2023-02-12 02:19:12 +0300

    Created solving 4 tasks

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает число и выдаёт,
+//  является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число >");
+
+string value = System.Console.ReadLine();
+System.Console.WriteLine($"ВВЕДЕНО {value}");
+
+int a = Convert.ToInt32(value);
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Введеное число является ЧЕТНЫМ");
+}
+
+else
+{
+   System.Console.WriteLine("Введеное число является HEчетным");  
+}
