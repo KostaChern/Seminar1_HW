cf15893c82df105eaf581765c49b1bba3fb4d336
Konstantin Chern <codahl@yandex.ru>
2023-02-12 12:42:15 +0300

    Let's start solving the problem

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число >");
+
+string value = System.Console.ReadLine();
+System.Console.WriteLine($"ВВЕДЕНО {value}");
+
+int a = Convert.ToInt32(value);
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Введеное число является ЧЕТНЫМ");
+}
+
+else
+{
+   System.Console.WriteLine("Введеное число является HEчетным");  
+}
+
